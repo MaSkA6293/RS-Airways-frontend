1be2311e0a213651e2ab4de04e181c25bd4328f8
Oleg Shkalikov <maska6293@yandex.ru>
2023-07-14 15:58:03 +0300

    refactor: change structure of the header component

--- rs-airways-frontend/src/app/core/core.module.ts
+++ rs-airways-frontend/src/app/core/core.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoComponent } from './components/logo/logo.component';
-import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
+import { LogoComponent } from './components/header/components/logo/logo.component';
+import { HeaderMenuComponent } from './components/header/components/header-menu/header-menu.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
-import { SetDateFormatComponent } from './components/set-date-format/set-date-format.component';
-import { SetCurrencyComponent } from './components/set-currency/set-currency.component';
-import { MenuRegularComponent } from './components/menu-regular/menu-regular.component';
-import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
-import { DateFormatItemComponent } from './components/date-format-item/date-format-item.component';
-import { CurrencyItemComponent } from './components/currency-item/currency-item.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SetDateFormatComponent } from './components/header/components/set-date-format/set-date-format.component';
+import { SetCurrencyComponent } from './components/header/components/set-currency/set-currency.component';
+import { MenuRegularComponent } from './components/header/components/menu-regular/menu-regular.component';
+import { SignInComponent } from './components/header/components/sign-in/sign-in.component';
+import { CurrencyItemComponent } from './components/header/components/currency-item/currency-item.component';
+import { DateFormatItemComponent } from './components/header/components/date-format-item/date-format-item.component';
+import { MenuMobileComponent } from './components/header/components/menu-mobile/menu-mobile.component';
 
 @NgModule({
   declarations: [
